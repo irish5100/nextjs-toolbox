1326ef81b077b952cfe4aa85acc68350b1dd8c36
irish5100 <81332856+irish5100@users.noreply.github.com>
2022-05-12 19:31:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-<?php
-$vist_page     =   "index.php";
-include "logger.php";
-?>
-
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";

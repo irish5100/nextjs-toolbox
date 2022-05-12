e349159ca429de3efd35ba468d5759cab135eeb9
irish5100 <81332856+irish5100@users.noreply.github.com>
2022-05-12 19:15:45 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,12 @@ export default function Header({ title }) {
     <>
       <h1 className="title">{title}</h1>
       <p>
-        Hi 👋! This site allows you to view some known hackers our
-        team has detected online.
+        Hi 👋! This site allows you to view some known hackers our team has detected online.
       </p>
 
       <p>
       You can report a hacker by filling in the form at the botoom of the page or joining our <a href="https://nextjs.org/">discord server</a>.
-        </a>
+
         Stay Safe!
       </p>
     </>

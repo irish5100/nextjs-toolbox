593f66b205fc4f274ac9767e7eae8e01b45da815
irish5100 <81332856+irish5100@users.noreply.github.com>
2022-05-12 19:25:58 +0100

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -5,20 +5,20 @@ export default function FeedbackForm() {
       <form
         className={styles.form}
         data-netlify="true"
-         data-netlify-recaptcha="true"
-//        data-netlify-honeypot="bot-field"
+//         data-netlify-recaptcha="true"
+        data-netlify-honeypot="bot-field"
         name="feedback"
         method="POST"
         action="/success"
       >
         <input type="hidden" name="form-name" value="feedback" />
-          /*
+          
         <p className={styles.hidden}>
             <label>
             Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
         </p>
-        */
+        
   
         <label htmlFor="name">Your Name</label>
         <input id="name" className={styles['form-field']} type="text" name="name" />
@@ -28,7 +28,7 @@ export default function FeedbackForm() {
 
         <label htmlFor="feedback">Known Hacker Details Or Hacker Removal Details</label>
         <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
-        <div data-netlify-recaptcha="true"></div>
+//        <div data-netlify-recaptcha="true"></div>
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
